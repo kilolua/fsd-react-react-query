f6c6f35b77b5edb9b4dbeb073f048d3d33f10a9b
ngladkov <ngladkov@tm-serv.ru>
2024-04-05 15:10:47 +0300

    initial commit

--- frontend/src/1-app/router/index.tsx
+++ frontend/src/1-app/router/index.tsx
@@ -1,7 +1,7 @@
-import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom";
+import { createHashRouter, createRoutesFromElements, Route} from "react-router-dom";
 import {OrdersPage} from "@/3-pages/ordersPage";
 
-export const router = createBrowserRouter(createRoutesFromElements(
+export const router = createHashRouter(createRoutesFromElements(
     <>
         <Route path={'orders'} element={<OrdersPage/>}></Route>
     </>
